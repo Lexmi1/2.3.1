8fef24ca6f54ce1d1c94698382d6979280921160
Strukov Alexandr <sasha_strukov_00@mail.ru>
2022-10-19 13:04:45 +0300

    2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,21 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarService;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarServiceImp;
 
 @Controller
 public class CarController {
 
-    private CarService carService = new CarService();
+    private CarServiceImp carServiceImp = new CarServiceImp();
 
     @GetMapping(value = "/cars")
     public String getCar(@RequestParam(defaultValue = "5") int count, Model model) {
 
-        model.addAttribute("carList", carService.getCar(count));
+        model.addAttribute("carList", carServiceImp.getCar(count));
 
         return "cars";
     }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,34 +1,10 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class CarService {
+public interface CarService {
+    List<Car> getCar(int kol);
 
-    private List<Car> carList;
-
-    {
-        carList = new ArrayList<>();
-        carList.add(new Car("Toyota", 1, "Germany"));
-        carList.add(new Car("Lada", 2, "Russia"));
-        carList.add(new Car("Lada", 3, "Russia"));
-        carList.add(new Car("Mercedes", 4, "Germany"));
-        carList.add(new Car("Ferrari", 5, "France"));
-        carList.add(new Car("Bugatti", 6, "France"));
-        carList.add(new Car("Volga", 7, "Russia"));
-        carList.add(new Car("Volga", 8, "Russia"));
-    }
-
-    public List<Car> getCar(int kol) {
-
-        if (kol >= 5) {
-            return carList;
-        } else {
-            return carList.stream().limit(kol).toList();
-        }
-    }
 }
